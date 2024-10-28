85f311780a11e1122368da01a8b541d5d0446515
Forest Anderson <forestkzanderson@gmail.com>
2024-10-28 18:22:29 -0400

    Fix DMX data offset

--- src/dmx/mod.rs
+++ src/dmx/mod.rs
@@ -35,7 +35,11 @@ impl DmxState {
 
     pub fn set_dmx_state(&mut self, dmx_state_var_positions: Vec<DmxStateVarPosition>) {
         for (index, value) in dmx_state_var_positions {
-            self.values[index as usize] = value;
+            // DMX addresses start at 1, not 0. Translate the DMX index to the
+            // correct array index
+            let index = index as usize - 1;
+
+            self.values[index] = value;
         }
     }
 
@@ -53,15 +57,13 @@ impl DmxState {
                     let mut data = Vec::new();
 
                     // Add the header to the start of the array
+                    // TODO: Set this up correctly
                     data.push(0xA0);
 
                     // Add the rest of the values
                     data.extend_from_slice(&self.values);
 
-                    uart_tx
-                        .send(UartMessage::DMX(data))
-                        .await
-                        .unwrap();
+                    uart_tx.send(UartMessage::DMX(data)).await.unwrap();
                 }
                 DmxMessage::UpdateState(state) => {
                     for (index, value) in state {
@@ -88,105 +90,3 @@ impl Debug for DmxState {
         Ok(())
     }
 }
-
-// #[derive(PartialEq, Clone, Debug)]
-// pub struct FrameSendPack {
-//     pub header: DmxFrame,
-//     pub dmx_channel_data: Vec<DmxFrame>,
-// }
-
-// impl FrameSendPack {
-//     pub fn into_bytes(self) -> Vec<u8> {
-//         let mut bytes = Vec::new();
-
-//         bytes.push(self.header);
-//         for channel_data in self.dmx_channel_data {
-//             bytes.push(channel_data);
-//         }
-
-//         // Add extra bytes to pad up to 51 total frames including the header and
-//         // draw instructions
-//         while bytes.len() < 51 * 4 {
-//             bytes.push(0);
-//         }
-
-//         bytes
-//     }
-// }
-
-// #[derive(PartialEq, Clone, Debug)]
-// pub struct DmxMessageSendPack {
-//     pub header: DmxHeaderPack,
-//     pub channel_data: [DmxDataPack; DMX_CHANNELS],
-// }
-
-// impl Display for DmxMessageSendPack {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         // Show the data in the format:
-//         // Controller ID: 0x0A, Universe: 0x00
-//         // FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF
-//         // ... (255 bytes of channel data)
-//         writeln!(
-//             f,
-//             "Controller ID: 0x{:02X}, Universe: 0x{:02X}",
-//             self.header.controller_id.to_be(),
-//             self.header.universe.to_be()
-//         )?;
-
-//         for (i, data_pack) in self.channel_data.iter().enumerate() {
-//             if i % 16 == 0 && i != 0 {
-//                 writeln!(f)?;
-//             }
-//             write!(f, "{:02X} ", data_pack.channel_data.to_be())?;
-//         }
-
-//         // Pad with zeros if less than 255 bytes of channel data
-//         for i in self.channel_data.len()..DMX_CHANNELS {
-//             if i % 16 == 0 && i != 0 {
-//                 writeln!(f)?;
-//             }
-//             write!(f, "00 ")?;
-//         }
-
-//         // Add information about the projector and task
-//         let projector = match self.header.controller_id.to_be() {
-//             15 => "all projectors".to_string(),
-//             id => format!("projector {}", id),
-//         };
-
-//         let task = if self.header.universe.to_be() == 0 {
-//             "a homing request".to_string()
-//         } else {
-//             format!("{} draw instructions", self.channel_data.len())
-//         };
-
-//         write!(f, "\nSending to {} with {}", projector, task)
-//     }
-// }
-
-// impl DmxMessageSendPack {
-//     pub fn new(header: DmxHeaderPack, channel_data: Vec<DmxDataPack>) -> Self {
-//         DmxMessageSendPack {
-//             header,
-//             channel_data: channel_data.try_into().unwrap(),
-//         }
-//     }
-// }
-
-// /// Change from a MessageSendPack to a FrameSendPack
-// impl From<DmxMessageSendPack> for FrameSendPack {
-//     fn from(msg: DmxMessageSendPack) -> FrameSendPack {
-//         debug!("{msg}");
-
-//         let pack = FrameSendPack {
-//             header: msg.header.pack_header().unwrap()[0],
-//             dmx_channel_data: msg
-//                 .channel_data
-//                 .into_iter()
-//                 .map(|x| x.pack_data().unwrap()[0])
-//                 .collect(),
-//         };
-
-//         pack
-//     }
-// }
